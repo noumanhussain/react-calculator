5adef1f3d3efd2a5f32bffcfc3a0c8523af6d1a3
noumanhussain <noumanzakar@gmail.com>
2025-01-03 23:53:22 +0500

    counter for addition and subtraction

--- counter/src/App.jsx
+++ counter/src/App.jsx
@@ -1,7 +1,29 @@
+import { useState } from "react";
+
 function App() {
+  let [count, setCount] = useState(10);
+  // let count = 15;
+
+  function addition() {
+    if (count === 20) {
+      alert("Counter cannot be greater than 20");
+      return;
+    }
+    setCount(count + 1);
+  }
+  function subtract() {
+    if (count === 0) {
+      alert("Counter cannot be less than 0");
+      return;
+    }
+    setCount(count - 1);
+  }
   return (
     <>
-      <h2>counter</h2>
+      <h2>Current counter value : {count} </h2>
+      <button onClick={addition}>Add</button>
+      <br />
+      <button onClick={subtract}>Subtract</button>
     </>
   );
 }
